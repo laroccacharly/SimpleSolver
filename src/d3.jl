6e54347321049d94ffd103ae6e8733ae78d38e5c
Charly Robinson La Rocca <charly.robinson-larocca@cirrelt.ca>
2019-11-21 09:20:05 -0500

    Devoir 3

--- src/d3.jl
+++ src/d3.jl
@@ -48,3 +48,89 @@ function d3_q5()
     @show d = hinv * [90;90]
     return nothing 
 end 
+
+
+function fibo_optim(f, n::Int, a::Number, b::Number)
+    @info "Fib optimization"
+    fib_numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+    fn(n) = fib_numbers[n + 1]
+    x_left(bk, ak, n, k) = round(bk - (fn(n - k)/fn(n - k +1)) * (bk - ak); digits=2)
+    x_right(bk, ak, n, k) = round(ak + (fn(n - k)/fn(n - k +1)) * (bk - ak); digits=2)
+    
+    k = 1 
+    ak = a 
+    bk = b 
+    while k <= n 
+        @show k 
+        @show fn(n - k)
+        @show fn(n - k +1)
+        @show fn(n - k)/fn(n - k +1)
+        @show xk_left = x_left(bk, ak, n, k)
+        @show xk_right = x_right(bk, ak, n, k)
+        @show f(xk_left)
+        @show f(xk_right)
+
+        if f(xk_left) < f(xk_right)
+            println("moving the b")
+            bk = xk_right
+        elseif f(xk_left) > f(xk_right)
+            println("moving the a")
+            ak = xk_left 
+        else 
+            println("Same f for left and right")
+            return xk_left 
+        end 
+        @show ak 
+        @show bk 
+        @show k += 1 
+    end 
+end 
+
+function gold_optim(f, a::Number, b::Number, epsilon::Number)
+    @info "Gold optimization"
+    gold_number = 1.618 
+    x_left(bk, ak) = round(bk - (1/gold_number) * (bk - ak); digits=2)
+    x_right(bk, ak) = round(ak + (1/gold_number) * (bk - ak); digits=2)
+    k = 1 
+    ak = a 
+    bk = b 
+    d = Inf 
+    while d > epsilon
+        @show k 
+        @show d 
+        @show xk_left = x_left(bk, ak)
+        @show xk_right = x_right(bk, ak)
+        @show f(xk_left)
+        @show f(xk_right)
+
+        if f(xk_left) < f(xk_right)
+            println("moving the b")
+            bk = xk_right
+        elseif f(xk_left) > f(xk_right)
+            println("moving the a")
+            ak = xk_left 
+        else 
+            println("Same f for left and right")
+            return xk_left 
+        end 
+        @show ak 
+        @show bk 
+        d = bk - ak 
+        k += 1 
+    end 
+    @show d 
+    return nothing 
+end 
+
+export d3_q1
+f(x) = round(x * (x -5 *pi); digits=2) 
+function d3_q1() 
+    pi = 3.1416
+    a = 0 
+    b = 20
+    n = 7 
+    epsilon = 2
+    fibo_optim(f, n, a, b)
+    gold_optim(f, a, b, epsilon)
+
+end 
